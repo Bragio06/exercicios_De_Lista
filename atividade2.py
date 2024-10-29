e2c200759f9d0b2943ca39bf8e5ae20dea438221
Bagio06 <hbragio@gmail.com>
2024-10-29 20:55:48 -0300

    ok

---
+++ atividade2.py
@@ -0,0 +1,11 @@
+lista1 = []
+lista2 = []
+
+for i in range(5):
+    lista1.append(input("digie um numero para a lista 1: "))
+
+for i in range(10):
+    lista2.append(input("digie um numero para a lista 2: "))
+           
+lista3 = lista1 + lista2
+print(lista3)
