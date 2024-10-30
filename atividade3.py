5ad1af67a994d9dbb343250b875540a859e5c48b
Bagio06 <hbragio@gmail.com>
2024-10-30 19:41:30 -0300

    ok

--- atividade3.py
+++ atividade3.py
@@ -1,1 +1,16 @@
-lista
+lista_Nota = []
+lista_Matricula = []
+soma = 0
+
+for i in range(10):
+    nota = input("digite a nota: ")
+    lista_Nota.append(nota)
+    soma += nota
+
+media = soma / len(lista_Nota)
+
+for  i in range(10):
+    if lista_Nota[i] > media:
+        alto =         
+
+
